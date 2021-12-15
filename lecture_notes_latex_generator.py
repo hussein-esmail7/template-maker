216f0f166e168d253d70321a8f76a6ff5578de6c
Hussein Esmail <hussein.esmail7@gmail.com>
2021-12-14 21:49:12 -0500

    Finished Autogenerator for School Notes

--- lecture_notes_latex_generator.py
+++ lecture_notes_latex_generator.py
@@ -7,9 +7,12 @@ Description: This program generates a LaTeX template for School course notes
 '''
 
 import os
+import os.path
+import getopt
 import sys
 import re
 from datetime import datetime as date
+import datetime
 
 # ========= COLOR CODES =========
 color_end               = '\033[0m'
@@ -45,42 +48,119 @@ def yes_or_no(str_ask):
         else:
             print(f"{str_prefix_err} {error_neither_y_n}")
 
+def require_answer(q1):
+    q1ans = ""
+    while True:
+        q1ans = input(str_prefix_ques + " " + q1).strip()
+        if len(q1ans) == 0:
+            print(str_prefix_err + " You cannot provide a blank response!")
+        elif yes_or_no(f"Is this correct - '{q1ans}'? "):
+            break
+    return q1ans
+
 def next_weekday(d, weekday):
     days_ahead = weekday - d.weekday()
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
     return d + datetime.timedelta(days_ahead)
 
-def main():
+def index_str(list, search_str):
+    # Returns first index that contains string
+    for num, item in enumerate(list):
+        if search_str in item:
+            return num
+    return -1
+
+def main(argv):
     author = "Hussein Esmail"
-    fileName = sys.argv[-1] # Filename passed by templatemaker
-    courseCode = ""
-    while True:
-        courseCode = input("Course code (with spaces): ")
-        if yes_or_no(f"Is your code {courseCode} [y/n]? "):
-            break
+    filename = ""
+    path_template_dir = os.path.expanduser("~/git/templates/")
+    path_template_file = "lecture-template.tex"
+    currentDate = date.now().strftime("%Y %m %d")
+    lines_append = []
     weekdays = ""
-    while True:
-        weekdays = input("Input weekdays this course happens (MTWRF): ").strip().replace(" ", "")
-        if bool(re.match("^[MTWRFmtwrf]+$", weekdays)):
-            break
-        else:
-            print("Please only enter only MTWRF characters!")
+    courseCode = ""
+    courseLocation = ""
+    courseTitle = ""
+    courseSection = ""
     prof = ""
-    while True:
-        prof = input("Prof: ")
-        if yes_or_no(f"Is your prof {prof} [y/n]? "):
-            break
     semester = ""
-    while True:
-        semester = input("Year and Semester (Ex: 2021F): ")
-        if bool(re.match("^[0-9]{4}(W|F|SU)$", semester)):
-            break
-        else:
-            print("Please only enter in the correct format.")
+    courseCredits = ""
+    # https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+    try:
+        opts, args = getopt.getopt(argv,"ha:f:c:w:l:s:p:y:n:t:", ["author=", "filename=", "course-code=", "weekday=", "location=", "section=", "prof=", "semester=", "title="])
+    except getopt.GetoptError:
+        print('lecture_notes_latex_generator.py -a <author> -f <file name>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('lecture_notes_latex_generator.py -a <author> -f <file name>')
+            sys.exit()
+        elif opt in ("-a", "--author"):
+            author = arg
+        elif opt in ("-f", "--filename"):
+            filename = arg
+        elif opt in ("-c", "--course-code"):
+            courseCode = arg
+        elif opt in ("-w", "--weekday"):
+            weekdays = arg
+        elif opt in ("-l", "--location"):
+            courseLocation = arg
+        elif opt in ("-s", "--section"):
+            courseSection = arg
+        elif opt in ("-p", "--prof"):
+            prof = arg
+        elif opt in ("-y", "--semester"): # y for year as well
+            semester = arg
+        elif opt in ("-n", "--credits"): # n for number
+            courseCredits = int(arg)
+        elif opt in ("-t", "--title"): # Course title
+            courseTitle = arg
+
+    if courseCode == "":
+        courseCode = require_answer("Course code (with spaces): ")
+    if weekdays == "":
+        while True:
+            weekdays = input(str_prefix_ques + " Input weekdays this course happens (MTWRF): ").strip().replace(" ", "")
+            if bool(re.match("^[MTWRFmtwrf]+$", weekdays)):
+                break
+            else:
+                print("Please only enter only MTWRF characters!")
+    # Ask course location (room/building or REMT) courseLocation
+    if courseLocation == "":
+        courseLocation = require_answer("Location of this course: ")
+
+    # Ask for file name
+    if filename == "":
+        while True:
+            filename = input(str_prefix_ques + " File name (tex): ")
+            if not filename.endswith(".tex"):
+                filename += ".tex"
+            if yes_or_no("Is this correct - '" + filename + "'? "):
+                break
+
+    # TODO: Ask course title courseTitle
+    if courseTitle == "":
+        courseTitle = require_answer("Course title: ")
+    if courseSection == "":
+        while True:
+            courseSection = input(str_prefix_ques + " Course Section (1 char): ").strip()
+            if len(courseSection) != 1:
+                print(str_prefix_err + " The course code has to be 1 character only!")
+            elif yes_or_no("Is this correct - Section " + courseSection + "? "):
+                break
+    if prof == "":
+        prof = require_answer("Professor teaching this section: ")
+    if semester == "":
+        while True:
+            semester = input(str_prefix_ques + " Year and Semester (Ex: 2021F): ")
+            if bool(re.match("^[0-9]{4}(W|F|SU)$", semester)):
+                break
+            else:
+                print("Please only enter in the correct format.")
     # Confirming Week 1's start date
     week1monday = ""
-    year = semester[:4] # Get inputted year (first 4 characters)
+    year = int(semester[:4]) # Get inputted year (first 4 characters)
     partOfYear = semester[4:] # W or F of SU (rest of string after 4 characters)
     if partOfYear == "W":
         month = 1 # January
@@ -93,37 +173,81 @@ def main():
     else:
         d = datetime.date(date.now().year, 1, 7)
     next_monday = next_weekday(d, 0)
-    currentDate = date.now().strftime("%Y %m %d")
     while True:
         confirm_weekday = yes_or_no(f"Is the start of {semester} {next_monday.strftime('%Y %m %d')} [y/n]? ")
         if confirm_weekday:
             break
         else:
             print("NOT DONE - Incorrect start date")
+
+    if courseCredits == "":
+        while True:
+            try:
+                courseCredits = int(input(str_prefix_ques + " Course credit amount: "))
+                if yes_or_no("Is this correct - " + str(courseCredits) + " credits? "):
+                    break
+            except:
+                print("Your response caused an error!")
+
+    lines = open(path_template_dir + path_template_file, "r").readlines()
+    # Fix LaTeX preamble here
+    ncmd = "\\newcommand{\\"
+    lines[index_str(lines, "% [FILENAME]")] = "% " + filename + "\n"
+    lines[index_str(lines, "% Author: [AUTHOR]")] = "% Author: " + author + "\n"
+    lines[index_str(lines, "% Created: [DATE]")] = "% Created: " + currentDate + "\n"
+    lines[index_str(lines, "% Updated: [DATE]")] = "% Updated: " + currentDate + "\n"
+    lines[index_str(lines, "% Description: [DESCRIPTION]")] = "% Description: Course notes for " + courseCode + "\n"
+
+    lines[index_str(lines, ncmd + "myAuthor")] = ncmd + "myAuthor}{" + author + "}\n"
+    lines[index_str(lines, ncmd + "mySubject")] = ncmd + "mySubject}{Lecture notes for " + courseCode + " in " + semester + "}\n"
+    lines[index_str(lines, ncmd + "myKeywords")] = ncmd + "myKeywords}{" + courseCode + ", York University}\n"
+    lines[index_str(lines, ncmd + "myCourseDateCreated")] = ncmd + "myCourseDateCreated}{" + currentDate + "}\n"
+    lines[index_str(lines, ncmd + "myCourseDateUpdated")] = ncmd + "myCourseDateUpdated}{" + currentDate + "}\n"
+    lines[index_str(lines, ncmd + "myCourseCredits")] = ncmd + "myCourseCredits}{" + str(courseCredits) + "}\n"
+    lines[index_str(lines, ncmd + "myCourseCode")] = ncmd + "myCourseCode}{" + courseCode + "}\n"
+    lines[index_str(lines, ncmd + "myCourseTitle")] = ncmd + "myCourseTitle}{" + courseTitle + "}\n"
+    lines[index_str(lines, ncmd + "myCourseProf")] = ncmd + "myCourseProf}{" + prof + "}\n"
+    lines[index_str(lines, ncmd + "myCourseSemester")] = ncmd + "myCourseSemester}{" + semester + "}\n"
+    lines[index_str(lines, ncmd + "myCourseSchedule")] = ncmd + "myCourseSchedule}{" + weekdays + "}\n"
+    lines[index_str(lines, ncmd + "myCourseSection")] = ncmd + "myCourseSection}{" + courseSection + "}\n"
+    lines[index_str(lines, ncmd + "myCourseLocation")] = ncmd + "myCourseLocation}{" + courseLocation + "}\n"
+    
+    line_insert = index_str(lines, "% TODO: Lecture notes here")
+
+    # Print sections by week here
     lectureCount = 1
     for weekNum in range(1, 12): # There are normally 12 weeks in a semester
         # TODO: Account for skipping reading week
-        print("\n\\section{Week " + str(weekNum) + "}") # Each week section line
+        lines_append.append("\n\\section{Week " + str(weekNum) + "}") # Each week section line
         for lectureDay in weekdays: # Generating each subsection for every weekday in the week
             if lectureDay == "M":
                 d = next_weekday(d, 0) # 0 = Monday
+                weekday_short = "Mon"
             elif lectureDay == "T":
                 d = next_weekday(d, 1) # 1 = Tuesday
+                weekday_short = "Tue"
             elif lectureDay == "W":
                 d = next_weekday(d, 2) # 2 = Wednesday
+                weekday_short = "Wed"
             elif lectureDay == "R": # R = How York represents Thursday since T is used by Tuesday
                 d = next_weekday(d, 3) # 3 = Thursday
+                weekday_short = "Thu"
             elif lectureDay == "F":
                 d = next_weekday(d, 4) # 4 = Friday
+                weekday_short = "Fri"
             tDateFormatted = d.strftime("%Y %m %d")
-            print("\\subsection{Lecture " + str(lectureCount) + ": " + tDateFormatted + "}")
-            print("\\begin{itemize*}")
-            print("\t\\item ")
-            print("\\end{itemize*}\n")
+            lines_append.append("\\subsection{Lecture " + str(lectureCount) + ": " + tDateFormatted + " (" + weekday_short + ")}")
+            lines_append.append("\\begin{itemize*}")
+            lines_append.append("    \\item ")
+            lines_append.append("\\end{itemize*}\n")
             lectureCount += 1 # Increase lecture counter for the subsections
     # TODO: Place newly generated lines here
+    lines[line_insert:line_insert] = [line + "\n" for line in lines_append]
+    print("".join(lines))
     sys.exit()
 
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 0:
+        main(sys.argv[1:])
+    else:
+        main()
